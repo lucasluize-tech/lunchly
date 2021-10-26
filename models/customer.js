f0dda40d034ead8f0fa4d2a658ca66ebcf77adb7
lucas <lucasluize.tech@gmail.com>
2021-10-26 14:07:45 -0400

    added search functionality and Loyal Customers

--- models/customer.js
+++ models/customer.js
@@ -55,6 +55,38 @@ class Customer {
 
     return new Customer(customer);
   }
+  
+  static async getByName(name){
+    const results = await db.query(`SELECT id, 
+      first_name AS "firstName",  
+      last_name AS "lastName", 
+      phone, 
+      notes 
+     FROM customers WHERE first_name ILIKE $1`, [`${name}`]);
+    
+    const customers = results.rows.map(c => new Customer(c));
+    
+    if(customers.length < 1){
+      const err = new Error(`No customers with that name: ${name}`);
+      err.status = 404;
+      throw err;
+    }
+   
+    return customers;
+  }
+  
+  static async getBestCustomers(){
+    const results = await db.query(`SELECT customer_id, count(*)
+                                   FROM reservations
+                                   GROUP BY customer_id
+                                   ORDER BY count DESC
+                                   LIMIT 10`)
+    
+    console.log(results.rows)
+    const customers = results.rows.map(c => Customer.get(c.customer_id))
+    const loyalCustomers = await Promise.all(customers)
+    return loyalCustomers;                              
+  }
 
   /** get all reservations for this customer. */
 
@@ -64,6 +96,7 @@ class Customer {
   
   async getReservationById(id){
     return await Reservation.getReservationByIdForCustomer(id);
+    
   }
 
   /** save this customer. */
